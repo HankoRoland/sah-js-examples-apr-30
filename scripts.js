cf4d969f025038ef51cb8a03c2d226f82ea454b6
HankoRoland <hanko.roland@gmail.com>
2020-04-30 21:54:56 +0200

    first 3 parts

---
+++ scripts.js
@@ -0,0 +1,43 @@
+console.log("JavaScript is working!");
+
+let colors = ["orange", "blue", "green", "purple"];
+
+let post= {
+    title: "May post",
+    description: "My first post, awesome",
+    likecount: 12,
+    comments: ["cool!", "nice job!"]
+};
+
+console.log(colors);
+console.log(post);
+
+let age = 14;
+
+if (age>18) {
+    console.log("you're an adult!");
+} else {console.log("you're a kid!");
+}
+
+for (let i= 0; i < 10; i++) {
+    console.log(i);
+    
+} 
+
+console.log(colors);
+
+let myfunction = x => { 
+    console.log(x)
+}
+
+colors.forEach (color => {
+    console.log(color);
+});
+
+console.log("this is the end of our code");
+
+
+
+
+
+
